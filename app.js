13daf6cba88d0f781d34ba0676e66ccaeb44202c
tyhuangg <anotherty2003@gmail.com>
2025-10-27 15:40:22 +0800

    更新 app.js

--- app.js
+++ app.js
@@ -12,20 +12,30 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // 公開靜態資料夾
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+// 公開靜態資料夾
+app.use(express.static(path.join(__dirname, 'public'), {
+  extensions: ['html']
+}));
+
 
 // --- 靜態頁面路由 ---
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
 
-app.get('/tutorial', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'tutorial.html'));
-});
+// app.get('/tutorial', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'tutorial.html'));
+// });
 
-app.get('/roles', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'roles.html'));
-});
+// app.get('/roles', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'roles.html'));
+// });
+
+// app.get('/teach', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'teach.html'));
+// });
+
 
 // --- 錯誤處理 (optional) ---
 app.use((req, res) => {
